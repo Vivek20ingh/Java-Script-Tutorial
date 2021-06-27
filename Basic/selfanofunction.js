03005f6a2671504b39515f0f4cdbdd6a6184a01c
Vivek Singh <vs2357766@gmail.com>
2021-06-27 17:29:30 +0530

    compeleted closure

---
+++ Basic/selfanofunction.js
@@ -0,0 +1,13 @@
+function sayHello()
+{
+    console.log("I say hello");
+    console.log("I again say hello");
+}
+sayHello();
+
+(function()
+{
+    console.log("I say hello");
+    console.log("I again say hello");
+})();
+
---
+++ intermediate/lexicalscoping.js
@@ -0,0 +1,36 @@
+function init()
+{
+    var firstName ="vivek"
+    function sayFirstName()
+    {
+        console.log(firstName);
+    }
+    return sayFirstName;
+}
+
+var nnn= init();
+
+///closure
+
+
+nnn();
+
+// exam
+
+function add(x)
+{
+    console.log("yes");
+    return function(y)
+    {
+        return x+y;
+    }
+}
+
+var add5 = add(4);
+var z=add5(50);
+console.log(z);
+
+z=add(4)(5);
+
+console.log(z);
+
